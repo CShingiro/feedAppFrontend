ff48cb7512ecbaef78741b5c314226acf5bdc919
CShingiro <cshingiro@sociallyradicalwebdesign.com>
2023-04-17 10:47:52 -0400

    Delete Feed Functionality

--- src/components/FeedCard.js
+++ src/components/FeedCard.js
@@ -5,7 +5,7 @@ import Moment from "react-moment";
 
 import { AppContext } from "../context/applicationContext";
 
-import { addFeedMetaDataApi } from "../util/ApiUtil";
+import { addFeedMetaDataApi, deleteFeedApi } from "../util/ApiUtil";
 
 const FeedCard = ({
     feedId,
@@ -16,7 +16,8 @@ const FeedCard = ({
     firstName,
     lastName,
     profilePicture,
-    feedMetaData = []
+    feedMetaData = [],
+    loadOnDelete = undefined,
 }) => {
     const formikRef = useRef();
 
@@ -56,7 +57,7 @@ const FeedCard = ({
         cLastName,
         cPicture,
         cComment,
-        cCreatedOn,
+        cCreatedOn
     }) => (
         <div className="text-black p-4 antialiased flex">
             <img className="rounded-full h-8 w-8 mr-2 mt-1 " src={cPicture} />
@@ -120,6 +121,17 @@ const FeedCard = ({
     const AddCommentSchema = Yup.object().shape({
         comment: Yup.string().required("Required"),
     });
+    const deleteFeed = async () => {
+        if (!isFetching) {
+            setIsFetching(true);
+
+            const apiResponse = await deleteFeedApi(token, feedId);
+            if (apiResponse.status === 1) {
+                loadOnDelete(0);
+                setIsFetching(false);
+            }
+        }
+    }
     return (
         <div className="bg-white shadow rounded-lg mb-5">
             <div className="flex w-full border-t border-gray-100">
@@ -146,7 +158,25 @@ const FeedCard = ({
                     </div>
                 </div>
             </div>
-            {/* {#DeleteFeedButton Section} */}
+            {loadOnDelete && (
+                <div
+                    className="flex flex-col justify-center m-5"
+                    onClick={deleteFeed}
+                >
+                    <span className="transition ease-out duration-300 hover:bg-gray-50 bg-gray-100 h-8 px-2 py-2 text-center rounded-full text-gray-100 cursor-pointer">
+                        <svg
+                            className="h-4 w-4 text-gray-500"
+                            fill="none"
+                            viewBox="0 0 24 24"
+                        >
+                            <path
+                                fill="currentColor"
+                                d="M7 21q-.825 0-1.413-.588T5 19V6H4V4h5V3h6v1h5v2h-1v13q0 .825-.588 1.413T17 21H7ZM17 6H7v13h10V6ZM9 17h2V8H9v9Zm4 0h2V8h-2v9ZM7 6v13V6Z"
+                            />
+                        </svg>
+                    </span>
+                </div>
+            )}
             <div className="border-b border-gray-100"></div>
             <div className="text-gray-400 font-medium text-sm mb-7 mt-6 mx-3 px-2">
                 <img className="rounded w-full" src={picture} />
